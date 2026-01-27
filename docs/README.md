d588ccdbf4b7acd5d46fc2eba19c8edc64c8a74a
pinkcar203 <prithikashrikannan@gmail.com>
2026-01-27 21:39:37 +0800

    Rename chatbot. Implement skeletal version

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Mickey User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Mickey.java
@@ -0,0 +1,13 @@
+public class Mickey {
+    public static void main(String[] args) {
+        String logo = "(o.o)\n"    // eyes
+                    + " >^<";      // nose and mouth
+        
+        System.out.println("----------------------------------------------");
+        System.out.println(" Hello! I'm Mickey\n" + logo + "\n");
+        System.out.println(" What can I do for you?");
+        System.out.println("----------------------------------------------");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("______________________________________________");
+    }
+}
