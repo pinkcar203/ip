ff70e2d1d71837e4ac8fd6b6d8373c5e88cf2ba6
pinkcar203 <prithikashrikannan@gmail.com>
2026-01-27 22:09:14 +0800

    Add user command  echo

--- src/main/java/Mickey.java
+++ src/main/java/Mickey.java
@@ -1,13 +1,30 @@
+package mickey;
+
+import java.util.Scanner;
+
 public class Mickey {
     public static void main(String[] args) {
-        String logo = "(o.o)\n"    // eyes
-                    + " >^<";      // nose and mouth
+        String logo = "( o.o )\n"    // animal face
+                    + "  >^< ";      
         
-        System.out.println("----------------------------------------------");
+        System.out.println("------------------------------------------");
         System.out.println(" Hello! I'm Mickey\n" + logo + "\n");
         System.out.println(" What can I do for you?");
-        System.out.println("----------------------------------------------");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("______________________________________________");
+        System.out.println("------------------------------------------");
+        
+        Scanner entry = new Scanner(System.in);
+        String chatEntry = entry.nextLine();
+                
+        while (!chatEntry.equals("bye")) {
+            System.out.println("------------------------------------------");
+            System.out.println("   " + chatEntry); //repeats it
+            System.out.println("------------------------------------------");
+            chatEntry = entry.nextLine();
+        }
+        
+        System.out.println("------------------------------------------");
+        System.out.println(" Bye. Hope to see you again soon! :D ");
+        System.out.println("------------------------------------------");
+        entry.close();
     }
 }
